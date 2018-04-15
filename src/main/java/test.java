9b2011e99a7fcc3b48424b29237b21fc5cd867b5
devjackie <sypioneerdev@gmail.com>
2018-04-15 13:51:29 +0900

    branch test 1 committed by devjackie on 2018.04.15

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -5,6 +5,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test 입니다.");
+        System.out.println("test1 입니다.");
     }
 
 }
